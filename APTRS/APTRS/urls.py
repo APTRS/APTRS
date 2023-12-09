b6c1a468142ffcb667400c64538d97f20986f58c
Sourav Kalal <kalalsourav18@gmail.com>
2023-12-09 16:42:16 +0530

    added index to login redirect

--- APTRS/APTRS/urls.py
+++ APTRS/APTRS/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include,re_path
 from django.conf import settings
 from django.conf.urls.static import serve
+from django.views.generic import RedirectView
 
 
 
@@ -11,6 +12,7 @@ urlpatterns = [
     path('api/customer/',include('customers.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT,}),
+    path('', RedirectView.as_view(url='/api/auth/login/', permanent=True)),
 ]
 
 #urlpatterns = urlpatterns + static('/media/', document_root = settings.MEDIA_ROOT)
