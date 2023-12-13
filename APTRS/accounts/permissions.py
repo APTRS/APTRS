e742dff4a935ef3ff8300a3414370397263c4b67
Sourav Kalal <kalalsourav18@gmail.com>
2023-12-13 09:27:49 +0530

    updated permission to work with class based views

--- APTRS/accounts/permissions.py
+++ APTRS/accounts/permissions.py
@@ -6,23 +6,30 @@ from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework import status
 from functools import wraps
-
+from rest_framework.views import APIView
 
 
 
 def custom_permission_required(allowed_permissions):
-    def decorator(func):
-        @wraps(func)
+    def decorator(view_func):
+        @wraps(view_func)
         def wrapper(request, *args, **kwargs):
-            if request.user.is_superuser:  # Grant all permissions to superuser/admin
-                return func(request, *args, **kwargs)
+            if hasattr(request, 'user') and request.user.is_superuser:  # Check if user is superuser/admin
+                return view_func(request, *args, **kwargs)
 
-            user_groups = request.user.groups.all()
+            if isinstance(request, APIView):  # Check if it's a class-based view
+                user_groups = request.request.user.groups.all()
+            else:  # Assume it's a function-based view
+                user_groups = request.user.groups.all()
+
             for group in user_groups:
                 group_permissions = CustomPermission.objects.filter(customgroup=group)
                 for permission in group_permissions:
                     if permission.name in allowed_permissions:  # Change 'name' to the actual attribute
-                        return func(request, *args, **kwargs)
+                        return view_func(request, *args, **kwargs)
+
             return Response({'error': 'Permission denied'}, status=status.HTTP_403_FORBIDDEN)
+
         return wrapper
+
     return decorator
