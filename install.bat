27bf8f693d86e57d668534a08e04e1ce227584cd
Sourav Kalal <kalalsourav18@gmail.com>
2023-12-24 18:02:51 +0530

    fixed version check in install.bat

--- install.bat
+++ install.bat
@@ -5,12 +5,21 @@ FOR /F "tokens=2-4 delims=." %%A IN ('python --version 2^>^&1') DO (
 )
 ECHO Installed Python version: %python_version%
 
-REM Remove spaces in version string for comparison
-SET python_version_no_spaces=%python_version: =%
+REM Split the version string into major, minor, and patch versions
+FOR /F "tokens=1-3 delims=." %%A IN ("%python_version%") DO (
+    SET python_major_version=%%A
+    SET python_minor_version=%%B
+    SET python_patch_version=%%C
+)
 
-REM Compare the major and minor version numbers
-IF "%python_version_no_spaces%" GEQ "3.8" (
-    echo Python 3.8 and above found
+REM Compare the major and minor versions
+IF %python_major_version% GEQ 3 (
+    IF %python_minor_version% GEQ 8 (
+        echo Python 3.8 and above found
+    ) ELSE (
+        echo Python version %python_version% is below 3.8, which is not supported.
+        exit /b
+    )
 ) ELSE (
     echo Python version %python_version% is below 3.8, which is not supported.
     exit /b
