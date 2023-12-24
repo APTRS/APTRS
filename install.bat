f0ef09ec3b8b509106cf82014203db009a3690aa
Sourav Kalal <kalalsourav18@gmail.com>
2023-12-24 18:14:29 +0530

    fixed error in version wheck for windows

--- install.bat
+++ install.bat
@@ -5,26 +5,25 @@ FOR /F "tokens=2-4 delims=." %%A IN ('python --version 2^>^&1') DO (
 )
 ECHO Installed Python version: %python_version%
 
-REM Split the version string into major, minor, and patch versions
-FOR /F "tokens=1-3 delims=." %%A IN ("%python_version%") DO (
-    SET python_major_version=%%A
-    SET python_minor_version=%%B
-    SET python_patch_version=%%C
+REM Split the version string into major and minor versions
+FOR /F "tokens=1-2 delims=." %%A IN ("%python_version%") DO (
+    SET /A python_major_version=%%A
+    SET /A python_minor_version=%%B
 )
 
-REM Compare the major and minor versions
-IF %python_major_version% GEQ 3 (
-    IF %python_minor_version% GEQ 8 (
-        echo Python 3.8 and above found
-    ) ELSE (
-        echo Python version %python_version% is below 3.8, which is not supported.
-        exit /b
-    )
+REM Check if the major version is 3
+IF %python_major_version% NEQ 3 (
+    echo Python version %python_version% is not in the 3.x series, which is not supported.
+    exit /b
+)
+
+REM Check if the minor version is 8 or above
+IF %python_minor_version% GEQ 8 (
+    echo Python 3.8 and above found
 ) ELSE (
     echo Python version %python_version% is below 3.8, which is not supported.
     exit /b
 )
-
 
   pip >nul 2>&1 && (
     echo pip3 is installed
