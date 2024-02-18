b35af87cb857bd5e1ee7b8cfd41e61a94459a026
Anof-cyber <kalalsourav18@gmail.com>
2024-02-18 12:22:56 +0530

    fixed error in env

--- APTRS/APTRS/settings.py
+++ APTRS/APTRS/settings.py
@@ -170,7 +170,7 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['POSTGRES_Name'],
+        'NAME': os.environ['POSTGRES_DB'],
         'USER': os.environ['POSTGRES_USER'],
         'PASSWORD': os.environ['POSTGRES_PASSWORD'],
         'HOST': os.environ['POSTGRES_HOST'],
