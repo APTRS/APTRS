c8f0b2ffdfeb8e46c05bad0667e08fcf88ab76e7
Anof-cyber <kalalsourav18@gmail.com>
2024-06-06 22:48:04 +0530

    added Project Type API

--- APTRS/configapi/views.py
+++ APTRS/configapi/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from .models import ReportStandard
-from .serializers import ReportStandardSerializer
+from .models import ReportStandard, ProjectType
+from .serializers import ReportStandardSerializer, ProjectTypeSerializer
 
 class ReportStandardCreateView(generics.CreateAPIView):
     queryset = ReportStandard.objects.all()
@@ -9,3 +9,13 @@ class ReportStandardCreateView(generics.CreateAPIView):
 class ReportStandardListView(generics.ListAPIView):
     queryset = ReportStandard.objects.all()
     serializer_class = ReportStandardSerializer
+
+
+
+class ProjectTypeCreateView(generics.CreateAPIView):
+    queryset = ProjectType.objects.all()
+    serializer_class = ProjectTypeSerializer
+
+class ProjectTypeListView(generics.ListAPIView):
+    queryset = ProjectType.objects.all()
+    serializer_class = ProjectTypeSerializer
