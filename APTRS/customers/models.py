ec825eca4d329efaaeb250df0dd17043acf566da
Anof-cyber <kalalsourav18@gmail.com>
2024-08-03 19:25:57 +0530

    added company delete validation for internal company

--- APTRS/customers/models.py
+++ APTRS/customers/models.py
@@ -6,3 +6,10 @@ class Company(models.Model):
     img = models.ImageField(upload_to='company')
     address = models.TextField()
     internal = models.BooleanField(default=False)
+
+    ### Internal company cannot be deleted, Deleting this remove all users, projects etc.
+    def delete(self, *args, **kwargs):
+        if not self.internal:
+            super(Company, self).delete(*args, **kwargs)
+        else:
+            print(f"Attempt to delete internal company '{self.name}' ignored.")
