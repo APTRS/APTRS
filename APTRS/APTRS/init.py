944f2a36ec477171d91c3b62b366441c5bc82849
Sourav Kalal <kalalsourav18@gmail.com>
2024-11-12 12:50:14 +0530

    fixed celery issues in docker

--- APTRS/APTRS/init.py
+++ APTRS/APTRS/init.py
@@ -1,5 +1,10 @@
 
 
+from __future__ import absolute_import, unicode_literals
+from .celery import app as celery_app
+
+__all__ = ('celery_app',)
+
 VERSION = '1.0'
 BANNER = """
 
--- scripts/worker.sh
+++ scripts/worker.sh
@@ -1,4 +1,6 @@
 
 
+cd APTRS
 
-celery -A APTRS worker --loglevel=info
+celery -A APTRS worker -l debug &
+celery -A APTRS beat -l debug &
