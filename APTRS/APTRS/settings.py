9c14de775a7a4562baabace13fe65fba58b9e576
Sourav Kalal <kalalsourav18@gmail.com>
2024-11-17 22:45:31 +0530

    separated media and static folder, removed unwanted images, fixed poc images public over ngnix

--- APTRS/APTRS/settings.py
+++ APTRS/APTRS/settings.py
@@ -310,16 +310,12 @@ if USE_S3:
 else:
     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
-
-MEDIA_PATH = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(MEDIA_PATH, 'media')
-Company_LOGO = os.path.join(MEDIA_URL, 'company')
+MEDIA_PATH = BASE_DIR
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CKEDITOR_UPLOAD_LOCATION = os.path.join(MEDIA_ROOT, 'poc')
-CKEDITOR_UPLOAD_URL = os.path.join(MEDIA_URL, 'poc')
-CKEDITOR_UPLOAD_PATH = "poc/"
 
 ALLOWED_TAGS = ['strong', 'em', 's', 'u', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'ul', 'li', 'img', 'a', 'span',
-                'code', 'blockquote', 'div', 'font', 'table', 'tr', 'td', 'th','pre','figure','figcaption','sup', 'input', 'thead', 'tbody', 'i', 'mark', 'label', 'sub','hr'
+                'code', 'blockquote', 'div', 'font', 'table', 'tr', 'td', 'th','pre','figure','figcaption','sup', 'input', 'thead', 'tbody', 'i', 'mark', 'label', 'sub',
                 ]
 
 LOGGING = {
