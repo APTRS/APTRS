329667305006fab5869de4ed3848619bf496616f
Sourav Kalal <kalalsourav18@gmail.com>
2024-11-18 14:35:51 +0530

    removed static and media from prod url

--- APTRS/APTRS/urls.py
+++ APTRS/APTRS/urls.py
@@ -16,16 +16,13 @@ urlpatterns = [
     path('api/auth/',include('accounts.urls')),
     path('api/customer/',include('customers.urls')),
     path('api/config/', include('configapi.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
-    re_path(r'^static-report/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    path('', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
     urlpatterns = urlpatterns + [
     path('admin/', admin.site.urls),
-    #path('ckeditor/upload/', ckeditor_views.upload, name='ckeditor_upload'),
-    #re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
-    #path('ckeditor/', include('ckeditor_uploader.urls')),
-    #path("__debug__/", include("debug_toolbar.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
+    re_path(r'^static-report/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    path('', TemplateView.as_view(template_name='index.html')),
+    path("__debug__/", include("debug_toolbar.urls")),
     ]
