ef66e04eabe25bd6b785b758185715b417bfd2f6
Sourav Kalal <kalalsourav18@gmail.com>
2024-12-07 18:01:26 +0530

    fixed null company error in project, Deleting Company now delete related project as well

--- APTRS/project/models.py
+++ APTRS/project/models.py
@@ -27,7 +27,7 @@ PROJECT_STATUS_CHOICES = [
 
 class Project(models.Model):
     name = models.CharField(max_length=100, unique = False, null = False, blank = False, default=None)
-    companyname = models.ForeignKey(Company, on_delete=models.SET_NULL,editable=False,blank=True,null=True)
+    companyname = models.ForeignKey(Company, on_delete=models.CASCADE,editable=False,blank=True,null=True)
     description = models.TextField(unique = False, null = False, blank = False, default=None,validators=[xss_validator])
     projecttype = models.CharField(max_length=100, unique = False, null = False, blank = False, default=None)
     startdate = models.DateField()
