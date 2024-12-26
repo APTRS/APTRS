5792ced2a9095cd77c826f9b232dfc5fa05af9af
Sourav Kalal <kalalsourav18@gmail.com>
2024-12-26 21:14:42 +0530

    Updated vite.config.js for build optimization

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer';
+import { splitVendorChunkPlugin } from 'vite';
 
 export default defineConfig({
   optimizeDeps: {
@@ -12,12 +13,31 @@ export default defineConfig({
       exclude: ['ckeditor5-custom-build']
     },
     sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('ckeditor5')) {
+              return 'ckeditor5';
+            }
+            if (id.includes('react')) {
+              return 'react';
+            }
+            if (id.includes('lodash')) {
+              return 'lodash';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
   },
   plugins: [
     react(),
+    splitVendorChunkPlugin(),
     visualizer({
       filename: './dist/stats.html',
-      open: true, // Automatically open the visualization in your default browser
+      open: false, // Automatically open the visualization in your default browser
     }),
   ],
   test: {
